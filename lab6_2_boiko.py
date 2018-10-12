a7067b684f2a8c26c3b31f66f0451d30a77dea58
Nickas47 <nick-rasmus@mail.ru>
2018-10-12 17:13:52 +0300

    Add files via upload

---
+++ lab6_2_boiko.py
@@ -0,0 +1,5 @@
+a = input('text: ')
+print(a[:5])
+print(a[:-3])
+print(a[::2])
+print(len(a))
---
+++ lab6_3_boiko.py
@@ -0,0 +1,6 @@
+a = input('text: ')
+x = input('text x: ')
+print(a.find(x, 0, len(a)))
+print(a.rfind(x, 0, len(a)))
+    
+
