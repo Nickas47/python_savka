fee1cd966f758b48bf5c1909e06e953a7f909fd6
Nickas47 <nick-rasmus@mail.ru>
2018-10-25 14:26:22 +0300

    Update lab9_files_boiko.py

--- Lab9/lab9_files_boiko.py
+++ Lab9/lab9_files_boiko.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 
+
 if os.path.exists('C:/lab9/Boiko'):
     save_path = 'C:/lab9/Boiko/'
     name = os.path.join(save_path,"v4.txt")
@@ -18,10 +19,12 @@ with open(name) as inp, open(nam, "w+") as out:
         out.write(el)
 handle.close
 
+i = input("Введіть нову букву: ")
+
 save_path = 'C:/lab9/Boiko/'
 na = os.path.join(save_path,"42.txt")
 with open(nam) as inp, open(na, "w+",encoding="utf-8") as out:
     a = inp.readline()
-    for el in a[0:1]:
+    for el in a.replace('.','\n' + i):
         print(el)
 handle.close
